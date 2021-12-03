d03c6d24b447639dafc094889fb204f191a81f80
NeoNeonilla <nelliafleurova@gmail.com>
2021-12-03 07:38:19 -0600

    index.js

--- config/connection.js
+++ config/connection.js
@@ -1,1 +1,16 @@
 const mysql = require("mysql2");
+
+
+const connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "Karapuz75",
+    database: "employees"
+  });
+  
+  console.log('Connected to the employee database.')
+  connection.connect(function (err) {
+    if (err) throw err;
+  });
+  
+  module.exports = connection;
---
+++ db/seed.sql
@@ -0,0 +1,9 @@
+const Sequelize = require('sequelize');
+
+const sequelize = process.env.JAWSDB_URL
+  ? new Sequelize(process.env.JAWSDB_URL)
+  : new Sequelize('blogger', 'root', '', {
+      host: 'localhost',
+      port: 3306,
+      dialect: 'mysql'
+    });
